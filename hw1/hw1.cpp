a73ef6b771c00930213009f963691e3377cf2a38
Evgenii Ptitsyn <eptitsyn@gmail.com>
2020-01-06 03:29:35 +0300

    somthng done

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "lib.h"
 
 using namespace std;
 
 int main(){
 	cout << "Hello, world!" << endl
-	<< "Version: " << endl;
+	<< "Version: " << version() << endl;
 	return 0;
 }
