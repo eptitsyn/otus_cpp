6345776b8adbaaa759ceec6af3e3bc0041e51b4b
Evgenii Ptitsyn <eptitsyn@gmail.com>
2020-02-14 17:24:06 +0300

    Revert "remove all files for review"
    This reverts commit 556256f1857466546004a221d2a6fc4ef8440899.

---
+++ hw2/ip_filter.cpp
@@ -0,0 +1,50 @@
+#include "ip_filter_lib.h"
+
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	try {
+		vector<ipaddr> ips;
+		for(std::string line; std::getline(std::cin, line);)
+        {
+            std::vector<std::string> v = split(line, '\t');
+            ips.push_back(ipaddr(v.at(0)));
+        }
+		sort(ips.rbegin(), ips.rend());
+
+		//output #1
+		for(auto &i : ips){
+			cout << i << endl;
+		}
+
+		auto fips = filterip(ips, 1);
+
+		//output #2
+		for(auto &i : fips){
+			cout << i << endl;
+		}
+
+		auto f2ips = filterip(ips, 46, 70);
+
+		//output #3
+		for(auto &i : f2ips){
+			cout << i << endl;
+		}
+
+		auto f3ips = filterip_any(ips, 46);
+
+		//output #4
+		for(auto &i : f3ips){
+			cout << i << endl;
+		}
+	}     
+    catch(const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+	return 0;
+}
