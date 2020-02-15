77e04ca5f8aa5a6b2489708b99673dcfa27095fc
Evgenii Ptitsyn <eptitsyn@gmail.com>
2020-02-15 19:24:00 +0300

    hw4

--- hw4/src/print_ip.cpp
+++ hw4/src/print_ip.cpp
@@ -1,48 +1,4 @@
-#include <iostream>
-#include <stdexcept>
-#include <vector>
-#include <experimental/iterator>
-
-using namespace std;
-
-
-struct print_ip{
-
-// //container
-// template<typename T, 
-//     typename Fake = 
-//         typename std::enable_if<std::is_array<T>::value, void>::type>
-// print_ip(T&& value) {
-//     cout << "container" << endl;
-// };
-
-//integer type
-template<typename T, 
-    typename Fake = 
-        typename std::enable_if<std::is_integral<typename std::remove_reference<T>::type>::value, void>::type>
-print_ip(T&& value) {
-    copy(
-        std::make_reverse_iterator(static_cast<const uint8_t*>(static_cast<const void*>(&value)) + sizeof(T)),
-        std::make_reverse_iterator(static_cast<const uint8_t*>(static_cast<const void*>(&value))),  
-        std::ostream_iterator<int>(std::cout, "."));
-    cout << '\b' << ' ' << endl;
-};
-
-print_ip(std::string&& value) {
-    cout << value << endl;
-};
-
-};
-// ostream& operator<<(ostream& os, const print_ip& ip){
-//     bool first = true;
-//     for(auto i = rbegin(ip.byte);i != rend(ip.byte); ++i)
-//     {
-//         if(first) {first = false;}
-//          else { os << ".";};
-//         os << +*i;
-//     }
-//     return os;
-// }
+#include "print_ip.h"
 
 int main(){
     print_ip(int(1234));
@@ -51,16 +7,10 @@ int main(){
     print_ip(int(2130706433));
     print_ip(long(8875824491850138409));
     print_ip(std::string("trololo"));
-    //print_ip(std::vector<int>{1, 2, 3, 4});
-    // print_ip(std::string("trololo"));
+    print_ip(std::vector<int>{1, 2, 3, 4});
+    print_ip(std::list<int>{12, 34, 12, 23});
+    print_ip(std::tuple<int, int, int, int>{0, 1, 2, 3});
+    print_ip(std::tuple<int>{42});
+    //print_ip(std::tuple<int, char, int, int>{34, 'A', 43, 23});
     return 0;
 }
-
-// - Печать адреса как 
-// - Печать адреса как 
-// - Печать адреса как 
-// - Печать адреса как 
-// - Печать адреса как 
-// - Печать адреса как 
-// - Печать адреса как std::list
-// - Опционально. Печать адреса как std::tuple
---
+++ hw4/src/print_ip.h
@@ -0,0 +1,55 @@
+/**
+ * Print Ip function library
+ */
+#pragma once
+#include "mytraits.h"
+#include "myostream_joiner.h"
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <experimental/iterator>
+#include <type_traits>
+#include <list>
+#include <tuple>
+
+//container
+template<typename T,
+    typename std::enable_if<!is_same<typename std::remove_reference<T>::type, std::string>::value && 
+    is_container<typename std::remove_reference<T>::type>::value>::type* = nullptr 
+        >
+void print_ip(T&& value) {
+    copy(begin(value), end(value), make_myostream_joiner(std::cout, "."));
+    std::cout << std::endl;
+}
+
+//string
+template<typename T,
+    typename std::enable_if<is_same<typename std::remove_reference<T>::type, std::string>::value>::type* = nullptr 
+        >
+void print_ip(T&& value) {
+    std::cout << value << std::endl;
+};
+
+//integer type
+template<typename T,
+    typename = 
+        typename std::enable_if<std::is_integral<typename std::remove_reference<T>::type>::value>::type
+        >
+void print_ip(T&& value) {
+    copy(
+        std::make_reverse_iterator(static_cast<const uint8_t*>(static_cast<const void*>(&value)) + sizeof(T)),
+        std::make_reverse_iterator(static_cast<const uint8_t*>(static_cast<const void*>(&value))),  
+        make_myostream_joiner(std::cout, "."));
+    std::cout << std::endl;
+};
+
+//tuple
+template<typename T,
+    typename std::enable_if<is_specialization_ofsame<typename std::remove_reference<T>::type, std::tuple>::value>::type* = nullptr 
+        >
+void print_ip(T&& value) {
+    std::apply([](auto&& a, auto&&... args) {{std::cout << a;}{((std::cout << '.' << args ), ...);};}, value);
+    std::cout << std::endl;
+
+}
