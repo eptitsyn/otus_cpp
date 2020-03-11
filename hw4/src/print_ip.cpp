9ed848fa607cd7038a604cf9bcf4e6fb407df188
Evgenii Ptitsyn <eptitsyn@gmail.com>
2020-03-11 14:18:00 +0300

    Revert "remove for review 3"
    This reverts commit 760c55ce1d59eff5887d672364b9d0825455d02e.

---
+++ hw4/src/print_ip.cpp
@@ -0,0 +1,19 @@
+#include "print_ip.h"
+
+/**
+ * Main print_ip function
+ */
+int main(){
+    print_ip(int(1234));
+    print_ip(char(-1));
+    print_ip(short(0));
+    print_ip(int(2130706433));
+    print_ip(long(8875824491850138409));
+    print_ip(std::string("trololo"));
+    print_ip(std::vector<int>{1, 2, 3, 4});
+    print_ip(std::list<int>{12, 34, 12, 23});
+    print_ip(std::tuple<int, int, int, int>{0, 1, 2, 3});
+    print_ip(std::tuple<int>{42});
+    //print_ip(std::tuple<int, char, int, int>{34, 'A', 43, 23});
+    return 0;
+}
---
+++ hw4/src/print_ip.h
@@ -0,0 +1,73 @@
+/**
+ * @file print_ip.h
+ * @author Evgenii Ptitsyn
+ * @brief File containing function for printing IP.
+ */
+
+/**
+ * Print Ip function library
+ */
+#pragma once
+#include "mytraits.h"
+#include "myostream_joiner.h"
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <experimental/iterator>
+#include <type_traits>
+#include <list>
+#include <tuple>
+
+/**
+ * PrintIp function
+ * @param Takes container
+ */
+template<typename T,
+    typename std::enable_if<!is_same<typename std::remove_reference<T>::type, std::string>::value && 
+    is_container<typename std::remove_reference<T>::type>::value>::type* = nullptr 
+        >
+void print_ip(T&& value) {
+    copy(begin(value), end(value), make_myostream_joiner(std::cout, "."));
+    std::cout << std::endl;
+}
+
+/**
+ * PrintIp function
+ * @param Takes container
+ */
+template<typename T,
+    typename std::enable_if<is_same<typename std::remove_reference<T>::type, std::string>::value>::type* = nullptr 
+        >
+void print_ip(T&& value) {
+    std::cout << value << std::endl;
+}
+
+/**
+ * \brief PrintIp function
+ * @param takes integral types
+ */
+template<typename T,
+    typename = 
+        typename std::enable_if<std::is_integral<typename std::remove_reference<T>::type>::value>::type
+        >
+void print_ip(T&& value) {
+    copy(
+        std::make_reverse_iterator(static_cast<const uint8_t*>(static_cast<const void*>(&value)) + sizeof(T)),
+        std::make_reverse_iterator(static_cast<const uint8_t*>(static_cast<const void*>(&value))),  
+        make_myostream_joiner(std::cout, "."));
+    std::cout << std::endl;
+}
+
+/**
+ * PrintIp function
+ * @param Takes tuple, but only of same type.
+ */
+template<typename T,
+    typename std::enable_if<is_specialization_ofsame<typename std::remove_reference<T>::type, std::tuple>::value>::type* = nullptr 
+        >
+void print_ip(T&& value) {
+    std::apply([](auto&& a, auto&&... args) {{std::cout << a;}{((std::cout << '.' << args ), ...);};}, value);
+    std::cout << std::endl;
+
+}
