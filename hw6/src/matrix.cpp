ad76b52b5f6f37b580760058d4306d6357a50882
Evgenii Ptitsyn <eptitsyn@gmail.com>
2020-04-16 18:18:07 +0300

    gtest for hw6

--- hw6/src/matrix.cpp
+++ hw6/src/matrix.cpp
@@ -14,6 +14,8 @@ class Matrix{
         MatrixElement(datastore& data, size_t row, size_t col) : data(data), row(row), col(col){};
         operator T(){ 
             return data.count({row, col}) > 0 ? data[{row, col}] : default_value;}
+        operator const T() const {
+            return data.count({row, col}) > 0 ? data[{row, col}] : default_value;}
         MatrixElement& operator=(T element){
             if(element != default_value)
                 data[{row, col}] = element;
@@ -25,11 +27,11 @@ class Matrix{
 
     class MatrixRow{
         datastore& data;
-        size_t rowindex;
+        size_t row;
     public:
-        MatrixRow(datastore& data, size_t rowindex): data(data), rowindex(rowindex){}; 
+        MatrixRow(datastore& data, size_t row): data(data), row(row){}; 
         MatrixElement operator[](size_t columnindex){
-            return MatrixElement(data, rowindex, columnindex);
+            return MatrixElement(data, row, columnindex);
         };
     };
 
@@ -41,6 +43,18 @@ public:
         bool operator!=(iterator& rhs){return iter != rhs.iter;}
         iterator& operator--(){ --iter; return *this; }
         iterator& operator++(){ ++iter; return *this; }
+        std::tuple<size_t, size_t, T> operator*(){ return {std::get<0>(iter->first), std::get<1>(iter->first), iter->second}; }
+    protected:
+        typename datastore::iterator iter;
+    };
+
+    class const_iterator : public std::iterator<std::bidirectional_iterator_tag, T>{
+    public:
+        const_iterator(typename datastore::iterator iter) : iter(iter){};
+        bool operator==(iterator& rhs){return iter == rhs.iter;}
+        bool operator!=(iterator& rhs){return iter != rhs.iter;}
+        const iterator& operator--() const { --iter; return *this; }
+        const iterator& operator++() const { ++iter; return *this; }
         std::tuple<size_t, size_t, T> operator*(){ return {std::get<0>(iter->first), std::get<1>(iter->first), iter->second}; }
     protected:
         typename datastore::iterator iter;
--- hw6/tests/gtest_unittest.cpp
+++ hw6/tests/gtest_unittest.cpp
@@ -1,18 +1,25 @@
-#include "print_ip.h"
+#include "matrix.cpp"
 
 #include "gtest/gtest.h"
-#include <sstream>
 
-TEST(ipfiltertest, inttest){
-	testing::internal::CaptureStdout();
-	print_ip(int(1234));
-	std::string output = testing::internal::GetCapturedStdout();
-	EXPECT_EQ("0.0.4.210\n",output);
-};
 
-TEST(ipfiltertest, stringtest){
-	testing::internal::CaptureStdout();
-	print_ip(std::string("trololo"));
-	std::string output = testing::internal::GetCapturedStdout();
-	EXPECT_EQ("trololo\n",output);
-};
+TEST(Matrixtest, operatorset){
+	Matrix<int, -1> matrix;
+	matrix[1][1] = -1;
+	EXPECT_EQ(matrix[1][1], -1);
+	EXPECT_EQ(matrix.size(), 0);
+	matrix[100][100] = 314;
+	EXPECT_EQ(matrix[100][100], 314);
+	EXPECT_EQ(matrix.size(), 1);
+}
+
+TEST(Matrixtest, tasktest){
+	Matrix<int, 0> matrix;
+	for(int i = 0; i < 10; ++i){
+		matrix[i][i] = i;
+	}
+	for(int i = 0; i < 10; ++i){
+		matrix[i][9 - i] = 9 - i;
+	}
+	EXPECT_EQ(matrix.size(), 18);
+}
