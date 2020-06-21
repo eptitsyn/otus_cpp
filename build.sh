7852895b389072b9a0d590a81813cb138afdbe73
Evgenii Ptitsyn <eptitsyn@gmail.com>
2020-06-21 20:10:58 +0300

    coverity

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-rm -rf ./build/*
-cd build
+set -euo pipefail
+rm -rf ./build
+mkdir -p build && cd build
 cmake ..
-make
+cmake --build .
+cmake --build . --target test
+cmake --build . --target package
+cmake --build . --target docs
+ctest -j $(nproc)
+#cpack -G DEB
