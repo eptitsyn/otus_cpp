ceb04725cd8b1770588a7d5e164bfd69a37ed230
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-08 20:02:55 +0000

    Deploy eptitsyn/otus_cpp to github.com/eptitsyn/otus_cpp.git:gh-pages

--- search/all_12.js
+++ search/all_12.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['undocumented',['undocumented',['../structdate_1_1detail_1_1undocumented.html',1,'date::detail::undocumented'],['../structdate_1_1detail_1_1undocumented.html#ac8435ba183f37a6ede05606695e6febd',1,'date::detail::undocumented::undocumented()']]],
   ['unspecified_5fmonth_5fdisambiguator',['unspecified_month_disambiguator',['../structdate_1_1detail_1_1unspecified__month__disambiguator.html',1,'date::detail']]],
-  ['update',['update',['../classObserver.html#ac75e4b339faeb3ea6fe0a01bf0b4a215',1,'Observer::update()'],['../classostreamObserver.html#a966ea8f62c6649226033bdb8c9dd2e80',1,'ostreamObserver::update()'],['../classfileObserver.html#a6b99a1d2afaa19e1facda43c9d7c7f10',1,'fileObserver::update()']]]
+  ['update',['update',['../classObserver.html#ac75e4b339faeb3ea6fe0a01bf0b4a215',1,'Observer::update()'],['../classostreamObserver.html#a966ea8f62c6649226033bdb8c9dd2e80',1,'ostreamObserver::update()'],['../classfileObserver.html#a608abc5ac88023501f57dea0b319ba53',1,'fileObserver::update()']]]
 ];
--- search/functions_10.js
+++ search/functions_10.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['undocumented',['undocumented',['../structdate_1_1detail_1_1undocumented.html#ac8435ba183f37a6ede05606695e6febd',1,'date::detail::undocumented']]],
-  ['update',['update',['../classObserver.html#ac75e4b339faeb3ea6fe0a01bf0b4a215',1,'Observer::update()'],['../classostreamObserver.html#a966ea8f62c6649226033bdb8c9dd2e80',1,'ostreamObserver::update()'],['../classfileObserver.html#a6b99a1d2afaa19e1facda43c9d7c7f10',1,'fileObserver::update()']]]
+  ['update',['update',['../classObserver.html#ac75e4b339faeb3ea6fe0a01bf0b4a215',1,'Observer::update()'],['../classostreamObserver.html#a966ea8f62c6649226033bdb8c9dd2e80',1,'ostreamObserver::update()'],['../classfileObserver.html#a608abc5ac88023501f57dea0b319ba53',1,'fileObserver::update()']]]
 ];
